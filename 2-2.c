deb16fe76ec60a91483c79952f6cbf3011f690c7
Hojun Im <ghwndla8094@gmail.com>
2023-04-29 17:27:32 +0900

    조건문

---
+++ 2-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int grade;
+	scanf("%d", &grade);
+	if(100>=grade && grade>=90) printf("A");
+	else if(90>grade && grade>=80) printf("B");
+    else if(80>grade && grade>=70) printf("C");
+    else if(70>grade && grade>=60) printf("D");
+	else printf("F");
+    return 0;
+}
