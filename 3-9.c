26a782252d9d8c84cbeb3b678f8e8f3a439cc054
Hojun Im <ghwnsdla8094@gmail.com>
2023-04-30 20:08:12 +0900

    반복문

--- 3-9.c
+++ 3-9.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	int i, j;
-	for(i=1; i<=5; i++){
-		for(j=1; j<=i; j++){
-			printf("*");
+	int i, n, j;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		for(j=0; j<i; j++){
+		printf("*");
 		}
 		printf("\n");
 	}
-    return 0;
+	return 0;
 }
