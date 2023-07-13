1c910cd7a24d1e6083a9e7efe1be5aa7aeaa3e5a
Hojun Im <ghwnsdla8094@gmail.com>
2023-07-13 20:14:29 +0900

    브루트 포스

---
+++ 12-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c, d, e, f, x, y, i, j;
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+	for(i=-999; i<=999; i++){
+		for(j=-999; j<=999; j++){
+			if((a*i+b*j==c) && (d*i+e*j==f)) printf("%d %d", i, j);
+		}
+	}
+	return 0;
+}
