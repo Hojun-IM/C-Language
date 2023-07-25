780cf2ef8713fd09733cec99b46dc028b0c8d044
Hojun Im <ghwnsdla8094@gmail.com>
2023-07-25 16:24:43 +0900

    정렬

---
+++ 13-4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int arr[1000000];
+
+int compare(const void* a, const void* b);
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	
+	for(int i=0; i<n; i++){
+		scanf("%d", &arr[i]);
+	}
+	qsort(arr, n, sizeof(int), compare);
+	for(int i=0; i<n; i++){
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
+
+int compare(const void* a, const void* b){
+	if(*(int*)a < *(int*)b) return -1;
+	else if(*(int*)a > *(int*)b) return 1;
+	return 0;
+}
